98b1fa483446a83722215e5fa0d6806de9d36275
AndreFilho0 <filhoandre534@gmail.com>
2024-04-21 22:30:50 -0300

    fix final dos filtros

--- resources/js/Pages/BuscarProfessor.vue
+++ resources/js/Pages/BuscarProfessor.vue
@@ -20,7 +20,7 @@
             <div>
                 <label for="professor" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome do Professor</label>
                 <select v-model="nomeProfessor" id="professor" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                    <option v-for="prof in Prof" :value="prof.nomeProfessor">{{ prof.nomeProfessor }}</option>
+                    <option v-for="prof in ProfExp" :value="prof.nomeProfessor">{{ prof.nomeProfessor }}</option>
                     <!-- Substitua 'prof.id' pelo valor real que representa o identificador único do professor -->
                 </select>
             </div>  
