4d2cbc11bfcd41694b17ac2e8380876825ff485a
AndreFilho0 <filhoandre534@gmail.com>
2024-04-21 22:43:59 -0300

    colocando rota de redirecao depois de dar nota e comentario

--- resources/js/Pages/DarNotaProfessor.vue
+++ resources/js/Pages/DarNotaProfessor.vue
@@ -35,7 +35,7 @@
           </button>
       </form>
       
-      <Link v-if="not" :href="route('dashboard')" type="button" class="relative  mt-10 block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      <Link v-if="not" :href="route('dashboard.buscarprofessor')" type="button" class="relative  mt-10 block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
     <span class="mt-2 block text-sm font-medium text-gray-900"><div v-html="mensagemAgradecimento" class="p-4 m-4"></div></span>
       </Link>
   </div>
