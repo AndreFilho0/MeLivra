8e0e0a07ab2b30678197409957ad2fc39bef049a
Thiago <thiagobossun06@gmail.com>
2024-11-15 17:12:46 -0300

    estilização das caixas

--- resources/js/Pages/ProfMaisBemAvaliado.vue
+++ resources/js/Pages/ProfMaisBemAvaliado.vue
@@ -1,17 +1,35 @@
 <!-- Componente para as tabelas professor melhor avaliado e Último Comentario -->
 <template>
   <div class="flex justify-between w-full mt-3 px-2">
-    <div class="justify-center w-full mx-2 rounded-lg border border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800">
+    <div class="
+        justify-center 
+        w-full 
+        mx-2 
+        rounded-lg 
+        border
+        p-2
+        shadow-xl
+        border-gray-300 
+        bg-gray-100">
       <h2 class="text-center text-2xl font-medium text-gray-900 dark:text-white">Professor melhor avaliado</h2>
       <p class="text-center">
         {{ professor.nomeProfessor }} é um professor legal.
       </p>
     </div>
 
-    <div class="justify-center w-full mx-2 rounded-lg border border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800">
+    <div class="
+        justify-center 
+        w-full 
+        mx-2 
+        rounded-lg 
+        border
+        p-2
+        shadow-xl
+        border-gray-300 
+        bg-gray-100">
       <h2 class="text-center text-2xl font-medium text-gray-900 dark:text-white">Último comentário</h2>
-      <p class="text-center" >
-        {{ comentario.comentario }}
+      <p class="text-center">
+        "{{ comentario.comentario }}" ao prof {{ comentario.nomeProfessor }} do {{ comentario.instProfessor }}
       </p>
     </div>
   </div>
@@ -20,13 +38,13 @@
 <script setup>
 import { usePage } from '@inertiajs/vue3';
 import { ref, onMounted } from 'vue';
-  const professor = ref({});
-  const comentario = ref({});
+const professor = ref({});
+const comentario = ref({});
 
-  onMounted(() => {
-    professor.value = usePage().props.professor || {}; 
-    comentario.value = usePage().props.comentario || {};
-    console.log(professor.value);
-    console.log(comentario.value);
-  });
+onMounted(() => {
+  professor.value = usePage().props.professor || {};
+  comentario.value = usePage().props.comentario || {};
+  console.log(professor);
+  console.log(comentario);
+});
 </script>
